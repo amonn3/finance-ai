151f0db957c0f046fae047a724fe4035f135f952
aamonnlimaa@gmail.com <aamonnlimaa@gmail.com>
2024-11-05 13:30:01 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/app/_components/ui/button";
+
 const Home = () => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div className="flex h-screen w-screen flex-col items-center justify-center">
       <h1 className="text-red-500">Dashboard Page</h1>
+      <Button>Clica no pae</Button>
     </div>
   );
 };
